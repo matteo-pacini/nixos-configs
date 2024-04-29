09bb2805d6f46ac424d583f722af4b9e538ec33f
matteo-pacini <matteo@codecraft.it>
2024-04-29 09:09:33 +0100

    Added mullvad-vpn

--- hosts/Brightfalls/services.nix
+++ hosts/Brightfalls/services.nix
@@ -5,8 +5,14 @@
 }: {
   services.flatpak.enable = true;
   services.fstrim.enable = true;
+
   virtualisation.docker.enable = true;
   virtualisation.docker.daemon.settings = {
     data-root = "/data/docker";
   };
+
+  # https://discourse.nixos.org/t/connected-to-mullvadvpn-but-no-internet-connection/35803/8?u=lion
+  services.resolved.enable = true;
+  services.mullvad-vpn.enable = true;
+  services.mullvad-vpn.package = pkgs.unstable.mullvad-vpn;
 }
