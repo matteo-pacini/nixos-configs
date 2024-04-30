e94f28da5ae31019159c73d0cccc668424d5e31b
Matteo Pacini <matteo@codecraft.it>
2024-04-30 22:58:03 +0100

    xcodes module: add Xcodes to user /Applications

--- modules/home-manager/darwin/xcodes.nix
+++ modules/home-manager/darwin/xcodes.nix
@@ -8,7 +8,7 @@ with lib; let
   cfg = config.programs.xcodes;
 in {
   options.programs.xcodes = {
-    enable = mkEnableOption "Xcodes service to manage multiple Xcode installations on macOS";
+    enable = mkEnableOption "Manages multiple Xcode installations on macOS";
     versions = mkOption {
       type = types.listOf types.str;
       default = ["15.3"];
@@ -32,15 +32,18 @@ in {
       export PATH="${lib.makeBinPath (with pkgs; [xcodes aria])}:$PATH"
 
       REQUIRED_XCODES="${concatStringsSep "\n" cfg.versions}"
-      INSTALLED_XCODES=$(xcodes installed | grep -oE '^[^\(]*' | sed 's/ (//g')
+      INSTALLED_XCODES=$(
+        NO_COLOR=1 xcodes installed --directory "${config.home.homeDirectory}/Applications" | \
+        grep -oE '^[^\(]*' || true
+      )
 
       echo -e "Requested versions:\n$REQUIRED_XCODES"
       echo -e "Installed versions:\n$INSTALLED_XCODES"
 
       for version in $REQUIRED_XCODES; do
         if ! echo $INSTALLED_XCODES | grep -q "$version"; then
-          echo "Installing Xcode $version"
-          NO_COLOR=1 xcodes install "$version"
+          echo "Installing Xcode $version..."
+          xcodes install --directory "${config.home.homeDirectory}/Applications" "$version"
         else
           echo "Xcode $version is already installed"
         fi
@@ -48,13 +51,13 @@ in {
 
       for version in $INSTALLED_XCODES; do
         if ! echo $REQUIRED_XCODES | grep -q "$version"; then
-          echo "Purging Xcode $version"
-          NO_COLOR=1 xcodes uninstall "$version"
+          echo "Purging Xcode $version..."
+          xcodes uninstall --directory "${config.home.homeDirectory}/Applications" "$version"
         fi
       done
 
       echo "Setting active Xcode to ${cfg.active}..."
-      NO_COLOR=1 xcodes select "${cfg.active}"
+      xcodes select --directory "${config.home.homeDirectory}/Applications" "${cfg.active}"
     '';
   };
 }
