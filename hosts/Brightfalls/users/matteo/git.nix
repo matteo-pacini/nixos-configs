11746992c63c7cde6b77d6fa5e610cf88003d213
Matteo Pacini <matteo@codecraft.it>
2024-05-05 19:41:55 +0100

    Brightfalls: better git configuration

--- hosts/Brightfalls/users/matteo/git.nix
+++ hosts/Brightfalls/users/matteo/git.nix
@@ -6,8 +6,20 @@
 }: {
   programs.git = {
     enable = true;
+    lfs.enable = true;
     package = pkgs.gitAndTools.gitFull;
     userName = "Matteo Pacini";
     userEmail = "matteo@codecraft.it";
+    extraConfig = {
+      init.defaultBranch = "master";
+      core.editor = "code --wait";
+      diff.tool = "vscode";
+      difftool.vscode.cmd = "code --wait --diff $LOCAL $REMOTE";
+      merge.tool = "vscode";
+      mergetool.vscode.cmd = "code --wait --merge $REMOTE $LOCAL $BASE $MERGED";
+    };
+    ignores = [
+      ".DS_Store"
+    ];
   };
 }
