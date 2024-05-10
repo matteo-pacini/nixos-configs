a0704f817b20b4a757822d864f7cba5bb4333fa5
Matteo Pacini <matteo@codecraft.it>
2024-05-10 13:39:15 +0100

    NightSprings: no desktop

--- hosts/NightSprings/system.nix
+++ hosts/NightSprings/system.nix
@@ -7,6 +7,8 @@
   system.defaults.NSGlobalDomain.AppleInterfaceStyle = "Dark";
   system.defaults.dock.magnification = true;
   system.defaults.dock.show-recents = false;
+
+  system.defaults.finder.CreateDesktop = false;
   system.defaults.finder.AppleShowAllExtensions = true;
 
   security.pam.enableSudoTouchIdAuth = true;
