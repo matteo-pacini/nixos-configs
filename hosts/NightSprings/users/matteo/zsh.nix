da18b2b59e43437e8ea3fb07a44c8d9866809509
Matteo Pacini <matteo@codecraft.it>
2024-05-13 00:28:31 +0100

    NightSprings: fzf color scheme

--- hosts/NightSprings/users/matteo/zsh.nix
+++ hosts/NightSprings/users/matteo/zsh.nix
@@ -14,7 +14,13 @@
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
-    defaultOptions = ["--preview 'bat --color=always {}'"];
+    defaultOptions = [
+      "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9"
+      "--color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9"
+      "--color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6"
+      "--color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
+      "--preview 'bat --color=always {}'"
+    ];
   };
 
   programs.bat = {
