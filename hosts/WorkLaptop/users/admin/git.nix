0d3ae2f43fbf050683ed43b801753fe68e00dc81
Matteo Pacini <matteo@codecraft.it>
2024-05-20 11:08:19 +0100

    Git config changes for WorkLaptop

--- hosts/WorkLaptop/users/admin/git.nix
+++ hosts/WorkLaptop/users/admin/git.nix
@@ -20,7 +20,7 @@
     lfs.enable = true;
     package = pkgs.gitAndTools.gitFull;
     userName = "Matteo Pacini";
-    userEmail = "matteo.pacini@transreport.co.uk";
+    userEmail = "matteo@codecraft.it";
     extraConfig = {
       init.defaultBranch = "master";
       core.editor = "code --wait";
@@ -30,6 +30,19 @@
       mergetool.vscode.cmd = "code --wait --merge $REMOTE $LOCAL $BASE $MERGED";
       core.sshCommand = "ssh -i ~/.ssh/github";
     };
+    includes = [
+      {
+        contents = {
+          user = {
+            email = "matteo.pacini@transreport.co.uk";
+          };
+          core = {
+            sshCommand = "ssh -i ~/.ssh/github_work";
+          };
+        };
+        condition = "gitdir:${config.home.homeDirectory}/Work/";
+      }
+    ];
     ignores = [
       ".DS_Store"
     ];
