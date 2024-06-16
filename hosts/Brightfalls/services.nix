f8c26f9e008bd0a37849f1c3ffd1ac9f562dced8
Matteo Pacini <matteo@codecraft.it>
2024-06-16 21:57:08 +0100

    BrightFalls(VM): added spice-vdagentd

--- hosts/Brightfalls/services.nix
+++ hosts/Brightfalls/services.nix
@@ -8,9 +8,9 @@
   services.flatpak.enable = true;
   services.fstrim.enable = lib.mkIf (!isVM) true;
 
-  virtualisation.docker = lib.mkIf (!isVM) {
+  virtualisation.docker = {
     enable = true;
-    daemon = {
+    daemon = lib.mkIf (!isVM) {
       settings = {
         "data-root" = "/data/docker";
       };
@@ -18,7 +18,10 @@
   };
 
   # https://discourse.nixos.org/t/connected-to-mullvadvpn-but-no-internet-connection/35803/8?u=lion
-  services.resolved.enable = lib.mkIf (!isVM) true;
-  services.mullvad-vpn.enable = lib.mkIf (!isVM) true;
+  services.resolved.enable = true;
+  services.mullvad-vpn.enable = true;
   services.mullvad-vpn.package = pkgs.unstable.mullvad-vpn;
+
+  # Clipboard
+  services.spice-vdagentd.enable = lib.mkIf isVM true;
 }
