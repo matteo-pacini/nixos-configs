53f014da44c556fb646a12dfee67d034920db592
Matteo Pacini <matteo@codecraft.it>
2024-06-22 22:47:01 +0100

    Nexus: network config

--- hosts/Nexus/networking.nix
+++ hosts/Nexus/networking.nix
@@ -1,8 +1,18 @@
-{ ... }:
+{ lib, ... }:
 {
   networking.hostName = "Nexus";
 
   networking.firewall.enable = false;
 
-  networking.networkmanager.enable = true;
+  networking.useDHCP = lib.mkDefault false;
+
+  networking.interfaces.eno1.ipv4.addresses = [
+    {
+      address = "192.168.7.7";
+      prefixLength = 24;
+    }
+  ];
+
+  networking.defaultGateway = "192.168.7.1";
+  networking.nameservers = [ "192.168.7.1" ];
 }
