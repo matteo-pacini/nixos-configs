82751e2ac2f93117e6b1674a7e3bf1e008c138b6
Matteo Pacini <matteo@codecraft.it>
2024-08-17 09:35:31 +0100

    Nexus: added debora user

--- hosts/Nexus/users.nix
+++ hosts/Nexus/users.nix
@@ -4,18 +4,27 @@
 
   programs.command-not-found.enable = true;
 
-  users.users."matteo" = {
-    isNormalUser = true;
-    extraGroups = [
-      "wheel"
-      "media"
-      "downloads"
-    ];
-    shell = pkgs.zsh;
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIID8wJBTqQWKLy0RxQDuw8PAvD/KwYxSBcWHS434E3ar NightSprings"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDL/dVnxSvd+NanEktj7P/XvBigagDGArj+EAc9Fj02/ WorkLaptop"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEDKbjllwk/2outyoF+hB1SrDmQD7X1ywrt17hvL5w6p BrightFalls"
-    ];
+  users.users = {
+    "matteo" = {
+      isNormalUser = true;
+      extraGroups = [
+        "wheel"
+        "media"
+        "downloads"
+      ];
+      shell = pkgs.zsh;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIID8wJBTqQWKLy0RxQDuw8PAvD/KwYxSBcWHS434E3ar NightSprings"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDL/dVnxSvd+NanEktj7P/XvBigagDGArj+EAc9Fj02/ WorkLaptop"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEDKbjllwk/2outyoF+hB1SrDmQD7X1ywrt17hvL5w6p BrightFalls"
+      ];
+    };
+    "debora" = {
+      isNormalUser = true;
+      shell = pkgs.zsh;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE6nOm/5SEJUywMWrIH+afYX5vduEJMkfF+7y5Ue9FUY debora@CauldronLake"
+      ];
+    };
   };
 }
