46f42aa87a6ee72d61c39bf4d44db2d2c145ca63
Matteo Pacini <m+github@matteopacini.me>
2024-09-08 22:09:13 +0100

    BrightFalls: cleanup 2

--- overlays/brightfalls.nix
+++ overlays/brightfalls.nix
@@ -5,19 +5,13 @@
       pkg:
       pkg.overrideAttrs (oldAttrs: {
         env = (oldAttrs.env or { }) // {
-          NIX_CFLAGS_COMPILE =
-            (oldAttrs.env.NIX_CFLAGS_COMPILE or "") + " -O2 -pipe -march=znver2 -mtune=znver2";
-          NIX_CXXFLAGS_COMPILE =
-            (oldAttrs.env.NIX_CFLAGS_COMPILE or "") + " -O2 -pipe -march=znver2 -mtune=znver2";
+          NIX_CFLAGS_COMPILE = (oldAttrs.env.NIX_CFLAGS_COMPILE or "") + " -O2 -march=znver2 -mtune=znver2";
+          NIX_CXXFLAGS_COMPILE = (oldAttrs.env.NIX_CFLAGS_COMPILE or "") + " -O2 -march=znver2 -mtune=znver2";
         };
       });
   in
   {
     reshade-steam-proton = super.callPackage ../packages/reshade-steam-proton.nix { };
-
-    qemu = optimizedForBrightFalls super.qemu;
-    mangohud = optimizedForBrightFalls super.mangohud;
-    mesa = optimizedForBrightFalls super.mesa;
 
   }
 )
