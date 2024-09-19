719cfa90c1c098baf1557455d73e013ff4624ca8
Matteo Pacini <m@matteopacini.me>
2024-09-19 13:19:59 +0100

    Back to VSCodium

--- hosts/shared/home-manager/vscode.nix
+++ hosts/shared/home-manager/vscode.nix
@@ -1,56 +1,58 @@
 { pkgs, ... }:
-let
-  vscode-utils = pkgs.vscode-utils;
-  perlnavigator = vscode-utils.extensionFromVscodeMarketplace {
-    name = "perlnavigator";
-    publisher = "bscan";
-    version = "0.8.15";
-    hash = "sha256-IClcFvP86NdCtAhwkPgwG4pbGGEou4h2YVul+AAuZow=";
-  };
-  dependi = vscode-utils.extensionFromVscodeMarketplace {
-    name = "dependi";
-    publisher = "fill-labs";
-    version = "0.7.9";
-    hash = "sha256-VsjISVDZGGh6/pf3Fd5g8pYDvWXA1+0oZKlQEGLBp4M=";
-  };
-in
+
 {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscode;
+    package = pkgs.vscodium;
     enableUpdateCheck = false;
     enableExtensionUpdateCheck = false;
     extensions =
       let
         vscext = pkgs.vscode-extensions;
       in
-      with vscext;
-      [
-        # Editor
-        editorconfig.editorconfig
-        mhutchie.git-graph
-        usernamehw.errorlens
-        vscext.eamodio.gitlens
-        # Live share
-        ms-vsliveshare.vsliveshare
-        # Copilot
-        github.copilot
-        github.copilot-chat
-        # Nix
-        jnoortheen.nix-ide
-        # Go
-        golang.go
-        # Shell
-        timonwong.shellcheck
-        # Rust
-        rust-lang.rust-analyzer
-        tamasfe.even-better-toml
-        dependi
-        # Perl
-        perlnavigator
-        # Yaml
-        redhat.vscode-yaml
-      ];
+      (
+        with vscext;
+        [
+          # Editor
+          editorconfig.editorconfig
+          mhutchie.git-graph
+          usernamehw.errorlens
+          vscext.eamodio.gitlens
+          # Live share
+          ms-vsliveshare.vsliveshare
+          # Nix
+          jnoortheen.nix-ide
+          # Go
+          golang.go
+          # Shell
+          timonwong.shellcheck
+          # Rust
+          rust-lang.rust-analyzer
+          tamasfe.even-better-toml
+          # Yaml
+          redhat.vscode-yaml
+        ]
+        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "copilot";
+            publisher = "GitHub";
+            version = "1.231.1112";
+            sha256 = "sha256-7Xqd7YeiDlR1ygc8vhPXqYQy7agHqypFIDbESr0htKk=";
+          }
+          {
+            name = "perlnavigator";
+            publisher = "bscan";
+            version = "0.8.15";
+            hash = "sha256-IClcFvP86NdCtAhwkPgwG4pbGGEou4h2YVul+AAuZow=";
+          }
+          {
+            name = "dependi";
+            publisher = "fill-labs";
+            version = "0.7.9";
+            hash = "sha256-VsjISVDZGGh6/pf3Fd5g8pYDvWXA1+0oZKlQEGLBp4M=";
+          }
+        ]
+      );
     mutableExtensionsDir = false;
     userSettings = {
       "nix.enableLanguageServer" = true;
