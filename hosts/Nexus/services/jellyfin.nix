d35169f35bb6c47f741bba0940d63a7c4e4bb97c
Matteo Pacini <m@matteopacini.me>
2024-10-16 12:39:16 +0100

    Nexus: optimized Jellyfin

--- hosts/Nexus/services/jellyfin.nix
+++ hosts/Nexus/services/jellyfin.nix
@@ -1,5 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+  nativeJellyfinFfmpeg =
+    (pkgs.jellyfin-ffmpeg.override {
+      ffmpeg_6-full = pkgs.ffmpeg_6-full.override ({
+        withHeadlessDeps = true;
+        withNvcodec = true;
+      });
+    }).overrideAttrs
+      (oldAttrs: {
+        NIX_CFLAGS_COMPILE = (oldAttrs.NIX_CFLAGS_COMPILE or "") + " -march=native -O2 -pipe";
+      });
+  jellyfin = pkgs.jellyfin.override ({ jellyfin-ffmpeg = nativeJellyfinFfmpeg; });
+in
 {
   users.users."jellyfin" = {
     extraGroups = [ "media" ];
@@ -7,6 +20,7 @@
 
   services.jellyfin = {
     enable = true;
+    package = jellyfin;
     group = "media";
   };
 }
