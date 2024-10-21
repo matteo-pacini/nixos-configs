0b2da20c43e048a0ae5599f8a120361144c471dd
Matteo Pacini <m@matteopacini.me>
2024-10-21 01:07:41 +0100

    Router: added Worklaptop SSH key

--- hosts/Router/users.nix
+++ hosts/Router/users.nix
@@ -14,6 +14,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB/374uncPCGejnyMojVd00DkPsECjggeZnMyrpAEKwO BrightFalls"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILGLYdoccZVwgzJMHC4xLtV64k/JePzJRlZ0LF8U/h31 NightSprings"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZ20/LITPU88Gg0u3ImBc6rr6uPKgRdRz2JWm6CWnV8 WorkLaptop"
       ];
     };
   };
