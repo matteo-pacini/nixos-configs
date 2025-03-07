e331b0b909f811ac3bf08c7e6c8317224c25ed60
Matteo Pacini <m+github@matteopacini.me>
2025-03-07 21:36:34 +0000

    Nexus: rsyncd removal

--- hosts/Nexus/networking.nix
+++ hosts/Nexus/networking.nix
@@ -18,17 +18,4 @@
 
   services.tailscale.enable = true;
 
-  services.rsyncd = {
-    enable = true;
-    settings = {
-      fabrizio = {
-        path = "/diskpool/fabrizio";
-        comment = "Fabrizio";
-        "read only" = false;
-        list = true;
-        uid = "fabrizio";
-        gid = "users";
-      };
-    };
-  };
 }
