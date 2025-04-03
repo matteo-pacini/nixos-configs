7d49079c66450ba89d5f15c8bb3971137c3844fb
Matteo Pacini <m+github@matteopacini.me>
2025-04-03 01:29:05 +0100

    nexus: dynamic PUID/PGID resolution for torrent container

--- hosts/Nexus/services/qbittorrent/default.nix
+++ hosts/Nexus/services/qbittorrent/default.nix
@@ -1,10 +1,12 @@
-{ config, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   downloadDir = "/downloads/torrent";
   configDir = "/var/lib/qbittorrent";
-  qbittorrentUser = config.users.users.qbittorrent.name;
-  qbittorrentGroup = config.users.groups.qbittorrent.name;
-  downloadsGroup = config.users.groups.downloads.name;
 in
 {
   imports = [
@@ -12,7 +14,6 @@ in
   ];
 
   users.users.qbittorrent = {
-    uid = 390;
     isSystemUser = true;
     group = "qbittorrent";
     extraGroups = [
@@ -20,13 +21,11 @@ in
     ];
   };
 
-  users.groups.qbittorrent = {
-    gid = 390;
-  };
+  users.groups.qbittorrent = { };
 
   systemd.tmpfiles.rules = [
-    "d ${downloadDir} 2770 ${qbittorrentUser} ${downloadsGroup}"
-    "d ${configDir}   0700 ${qbittorrentUser} ${qbittorrentGroup}"
+    "d ${downloadDir} 2770 qbittorrent downloads"
+    "d ${configDir}   0700 qbittorrent qbittorrent"
   ];
 
   # Overrides
@@ -43,13 +42,21 @@ in
   virtualisation.oci-containers.containers."torrent" = {
     environment = {
       "WEBUI_PORT" = "7777";
-      "PUID" = toString config.users.users.qbittorrent.uid;
-      "PGID" = toString config.users.groups.qbittorrent.gid;
       "TZ" = config.time.timeZone;
     };
     volumes = [
       "${downloadDir}:/downloads:rw"
       "${configDir}:/config:rw"
     ];
+    extraOptions = [
+      "-e=PUID"
+      "-e=PGID"
+    ];
   };
+
+  systemd.services."podman-torrent".script = lib.mkBefore ''
+    export PUID="$(${pkgs.coreutils}/bin/id -u qbittorrent)"
+    export PGID="$(${pkgs.getent}/bin/getent group downloads | cut -d: -f3)"
+  '';
+
 }
