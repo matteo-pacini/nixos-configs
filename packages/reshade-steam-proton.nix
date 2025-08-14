177c565205523bb30d06d862ebe89695ded6eed0
Matteo Pacini <m+github@matteopacini.me>
2025-08-14 23:35:03 +0100

    reshade-steam-proton derivation cleanup

--- packages/reshade-steam-proton.nix
+++ packages/reshade-steam-proton.nix
@@ -10,6 +10,16 @@
   wget,
   file,
 }:
+let
+  binPath = lib.makeBinPath [
+    wineWowPackages.minimal
+    p7zip
+    git
+    curl
+    wget
+    file
+  ];
+in
 
 stdenv.mkDerivation {
 
@@ -20,49 +30,26 @@ stdenv.mkDerivation {
     owner = "kevinlekiller";
     repo = "reshade-steam-proton";
     rev = "55d4a681c9389e20ab569234f01bf67dbd6866a7";
-    sha256 = "sha256-jVqeVIW5cIgRkK/V3HxN1RKcRb+LaFR7n8GHxvowW0I=";
+    hash = "sha256-jVqeVIW5cIgRkK/V3HxN1RKcRb+LaFR7n8GHxvowW0I=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
 
   installPhase = ''
     mkdir -p $out/bin
-    cp "$src/reshade-linux.sh" "$out/bin/._reshade-linux.sh"
-    cp "$src/reshade-linux-flatpak.sh" "$out/bin/._reshade-linux-flatpak.sh"
-    cp "$src/reshade-steam-proton.sh" "$out/bin/._reshade-steam-proton.sh"
-    makeWrapper "$out/bin/._reshade-linux.sh" \
-                "$out/bin/reshade-linux.sh" \
-                 --prefix PATH : "${
-                   lib.makeBinPath [
-                     wineWowPackages.minimal
-                     p7zip
-                     git
-                     curl
-                     wget
-                     file
-                   ]
-                 }"
-
-
-    makeWrapper "$out/bin/._reshade-linux-flatpak.sh" \
-                "$out/bin/reshade-linux-flatpak.sh" \
-                 --prefix PATH : "${
-                   lib.makeBinPath [
-                     wineWowPackages.minimal
-                     p7zip
-                     git
-                     curl
-                     wget
-                     file
-                   ]
-                 }"
-
+    cp $src/*.sh $out/bin/
+    wrapProgram "$out/bin/reshade-steam-proton.sh" \
+      --prefix PATH : "${binPath}"
+    wrapProgram "$out/bin/reshade-linux.sh" \
+      --prefix PATH : "${binPath}"
+    wrapProgram "$out/bin/reshade-linux-flatpak.sh" \
+      --prefix PATH : "${binPath}"
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Easy setup and updating of ReShade on Linux for games using wine or proton";
     homepage = "https://github.com/kevinlekiller/reshade-steam-proton";
     platforms = [ "x86_64-linux" ];
-    license = licenses.gpl2;
+    license = lib.licenses.gpl2;
   };
 }
