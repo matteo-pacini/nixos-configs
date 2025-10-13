8b345573370ff1b26bf2df00736247032dc46d3b
Matteo Pacini <m+github@matteopacini.me>
2025-10-13 08:21:46 +0100

    darwin hosts: firefox downgrade to 143.0.3 because of crashes

--- overlays/nightsprings.nix
+++ overlays/nightsprings.nix
@@ -15,5 +15,13 @@
     gtk3 = super.gtk3.overrideAttrs (oldAttrs: {
       patches = (oldAttrs.patches or [ ]) ++ [ ../patches/gtk3-tests-sincos.patch ];
     });
+
+    firefox = super.firefox.overrideAttrs (oldAttrs: {
+      version = "143.0.3";
+      src = super.fetchurl {
+        url = "mirror://mozilla/firefox/releases/143.0.3/source/firefox-143.0.3.source.tar.xz";
+        sha512 = "c092bd3aac79f856a804c908b76d40409ce052b00176269ea3029b5a3a6885d4d21ce26bd76c9ea13827ff75459b6b4b0566f5aa49035ac234ae5890c67845b0";
+      };
+    });
   }
 )
