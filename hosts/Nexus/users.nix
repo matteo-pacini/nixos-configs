9168e21c6f366ee18f92eef5eaa783b6f382f38e
Matteo Pacini <m+github@matteopacini.me>
2025-10-14 08:29:46 +0100

    nexus: authorized ssh keys update

--- hosts/Nexus/users.nix
+++ hosts/Nexus/users.nix
@@ -19,7 +19,7 @@
       shell = pkgs.zsh;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIID8wJBTqQWKLy0RxQDuw8PAvD/KwYxSBcWHS434E3ar NightSprings"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDL/dVnxSvd+NanEktj7P/XvBigagDGArj+EAc9Fj02/ WorkLaptop"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFPfaTw8AYPvjul32mIt64juaOn8wjmlJoplWxCzCZhi WorkLaptop"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEDKbjllwk/2outyoF+hB1SrDmQD7X1ywrt17hvL5w6p BrightFalls"
       ];
     };
