2290bb7120b6baf582c768127de2e79809864138
Matteo Pacini <m+github@matteopacini.me>
2025-11-14 08:24:51 +0000

    brightfalls: initial disko config 11

--- hosts/Brightfalls/hardware.nix
+++ hosts/Brightfalls/hardware.nix
@@ -22,40 +22,7 @@
     "usbhid"
     "sd_mod"
   ];
-  boot.initrd.kernelModules = lib.optionals (!isVM) [
-    "dm-snapshot"
-    "usb_storage"
-    "uas"
-    "usbcore"
-    "vfat"
-    "nls_cp437"
-    "nls_iso8859_1"
-  ];
-
-  # LUKS encryption configuration for physical machine
-  # Vault partition is unlocked with password at boot and must be available before other LUKS devices
-  # All other LUKS volumes (root, games1, games2) are auto-unlocked using keyfile from vault
-
-  # Ensure vault is mounted during initrd before other LUKS devices try to unlock
-  fileSystems."/vault".neededForBoot = lib.mkIf (!isVM) true;
-
-  boot.initrd.luks.devices = lib.mkIf (!isVM) {
-    cryptroot = {
-      device = "/dev/disk/by-partlabel/disk-a-os-disk-root";
-      keyFile = "/vault/luks.key";
-      allowDiscards = true;
-    };
-    cryptgames1 = {
-      device = "/dev/disk/by-partlabel/disk-b-games-disk-1-games1";
-      keyFile = "/vault/luks.key";
-      allowDiscards = true;
-    };
-    cryptgames2 = {
-      device = "/dev/disk/by-partlabel/disk-c-games-disk-2-games2";
-      keyFile = "/vault/luks.key";
-      allowDiscards = true;
-    };
-  };
+  boot.initrd.kernelModules = lib.optionals (!isVM) [ "dm-snapshot" ];
   boot.kernelModules = lib.optionals (!isVM) [ "kvm-amd" ];
 
   boot.kernelParams = lib.optionals (!isVM) [
@@ -66,5 +33,31 @@
   hardware.cpu.amd.updateMicrocode = lib.mkIf (!isVM) true;
   hardware.firmware = [ pkgs.linux-firmware ];
   hardware.enableRedistributableFirmware = true;
+
+  # Vault must be mounted during initrd before other LUKS devices can access the keyfile
+  fileSystems."/vault".neededForBoot = lib.mkIf (!isVM) true;
+
+  boot.initrd.luks.devices."cryptvault" = lib.mkIf (!isVM) {
+    device = "/dev/disk/by-partlabel/disk-a-os-disk-vault";
+    allowDiscards = true;
+  };
+
+  boot.initrd.luks.devices."cryptroot" = lib.mkIf (!isVM) {
+    device = "/dev/disk/by-partlabel/disk-a-os-disk-root";
+    keyFile = "/vault/luks.key";
+    allowDiscards = true;
+  };
+
+  boot.initrd.luks.devices."cryptgames1" = lib.mkIf (!isVM) {
+    device = "/dev/disk/by-partlabel/disk-b-games-disk-1-games1";
+    keyFile = "/vault/luks.key";
+    allowDiscards = true;
+  };
+
+  boot.initrd.luks.devices."cryptgames2" = lib.mkIf (!isVM) {
+    device = "/dev/disk/by-partlabel/disk-c-games-disk-2-games2";
+    keyFile = "/vault/luks.key";
+    allowDiscards = true;
+  };
 
 }
