f76194155b126ad7410c246b5cea3380ef50b45e
Matteo Pacini <m+github@matteopacini.me>
2026-01-08 22:42:27 +0000

    WorkLaptop: Use vscode-augment from nixpkgs

--- hosts/WorkLaptop/users/matteo.pacini/vscode.nix
+++ hosts/WorkLaptop/users/matteo.pacini/vscode.nix
@@ -6,13 +6,8 @@
   programs.vscode = {
     profiles = {
       default = {
-        extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-          {
-            name = "vscode-augment";
-            publisher = "augment";
-            version = "0.596.2";
-            hash = "sha256-T75GltdyN5/HSu/N5SvcrBypT0PV5bl4rfgYyOx388Q=";
-          }
+        extensions = with pkgs.vscode-extensions; [
+          augment.vscode-augment
         ];
       };
     };
