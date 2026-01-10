4065699133361a804d3bcdce3c164c4ff24c6ecf
Matteo Pacini <m+github@matteopacini.me>
2026-01-10 05:58:30 +0000

    overlay/brightfalls: optimize only QEMU for znver4
    Remove mangohud and brlaser overrides. QEMU is the only package
    that meaningfully benefits from Zen4-specific optimizations.

--- overlays/brightfalls.nix
+++ overlays/brightfalls.nix
@@ -17,12 +17,6 @@
   {
     reshade-steam-proton = super.callPackage ../packages/reshade-steam-proton.nix { };
 
-    # NOTE: mesa cannot be optimized with -march=znver4 because mesa_clc
-    # (a build-time tool) would be compiled with Zen4 instructions and fail
-    # to run on build machines that don't support them (SIGILL).
-    # Mesa's performance-critical paths are in GPU shaders anyway.
-    mangohud = optimizedForBrightFalls super.mangohud;
-
     qemu = optimizedForBrightFalls (
       super.qemu.override ({
         hostCpuTargets = [
@@ -32,12 +26,6 @@
         ];
       })
     );
-
-    brlaser = super.brlaser.overrideAttrs (oldAttrs: {
-      cmakeFlags = (oldAttrs.cmakeFlags or [ ]) ++ [
-        "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
-      ];
-    });
 
   }
 )
