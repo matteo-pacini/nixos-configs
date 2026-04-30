c43212a0002bb4763f34f5d702a0d4a3decb2574
Matteo Pacini <m+github@matteopacini.me>
2026-04-30 16:08:51 +0100

    fix(overlays): add openldap flaky test workaround and tokscale upstream tracking
    - Workaround openldap 2.6.13 test017-syncreplication-refresh failing in Nix
      sandbox, breaking wine/bottles builds on CauldronLake and BrightFalls.
      See: https://github.com/NixOS/nixpkgs/issues/514113
      See: https://github.com/NixOS/nixpkgs/issues/513245
    - Add upstream PR tracking comment for tokscale overlay, to be removed once
      https://github.com/NixOS/nixpkgs/pull/510494 is merged.

--- overlays/shared.nix
+++ overlays/shared.nix
@@ -9,5 +9,22 @@
   });
 
   # Token usage tracker for AI coding agents
+  # TODO: Remove local package + overlay once merged upstream.
+  #   tokscale: init at 2.0.26 (PR aims to add to nixpkgs)
+  #   https://github.com/NixOS/nixpkgs/pull/510494
   tokscale = super.callPackage ../packages/tokscale.nix { };
+
+  # Workaround: openldap 2.6.13 test017-syncreplication-refresh is flaky in the
+  # Nix sandbox and fails on x86_64-linux, breaking anything depending on wine
+  # (e.g. bottles, lutris).
+  # Affected hosts in this flake: CauldronLake, BrightFalls (x86_64)
+  # Upstream issues:
+  #   - openldap: test checks won't let it compile on x86_64
+  #     https://github.com/NixOS/nixpkgs/issues/514113
+  #   - Build failure: lutris-free
+  #     https://github.com/NixOS/nixpkgs/issues/513245
+  # TODO: Remove once upstream disables the flaky test or releases a fix.
+  openldap = super.openldap.overrideAttrs (old: {
+    doCheck = false;
+  });
 })
