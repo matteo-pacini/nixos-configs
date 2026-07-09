7c82a32e81d94e3e1c1a79df97a4ba50f5dd57be
Matteo Pacini <m+github@matteopacini.me>
2026-07-09 23:59:00 +0100

    feat(brightfalls): enable git commit signing; rotate nexus ssh key

--- hosts/Nexus/users.nix
+++ hosts/Nexus/users.nix
@@ -20,7 +20,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIID8wJBTqQWKLy0RxQDuw8PAvD/KwYxSBcWHS434E3ar matteo@NightSprings"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFPfaTw8AYPvjul32mIt64juaOn8wjmlJoplWxCzCZhi matteo.pacini@WorkLaptop"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG+kL6LcApeIKrvJQIsewG95Q2XyzrvUSyRvBC9Ip3y5 matteo@BrightFalls"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDu9ZMKYAfbVvEvu4KKwwGaUc3XT3FxZlSIgE1jOpN7G matteo@BrightFalls"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINm/ozPgRTmYmOVgkdNOw2deEOzBjoA4gGWLjWzrEC+u Pixel"
       ];
     };
